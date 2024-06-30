f801bacc21331399af40b3911ce1aafed69726d4
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-30 20:04:11 +0900

    권한 실제 확인 요청

--- app/src/main/java/com/neppplus/librarypractice/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.neppplus.librarypractice
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
@@ -9,9 +10,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.coroutine.TedPermission
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,22 +47,46 @@ class MainActivity : AppCompatActivity() {
         val callBtn = findViewById<Button>(R.id.callBtn);
 
         callBtn.setOnClickListener {
-            //라이브러리 활용 -> 전화 권한 확인 -> 실제 전화 연결
+            // 라이브러리 활용 -> 전화 권한 확인 -> 실제 전화 연결
             val permissionListener = object:PermissionListener{
                 override fun onPermissionGranted() {
-                    // 권한 승인
-                    val myUri = Uri.parse("tel:010-2222-3333")
-                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
-
-                    startActivity(myIntent)
+                    //                // 권한이 허용된 경우 수행할 작업
+                val myUri = Uri.parse("tel:010-6592-3201")
+                val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                startActivity(myIntent)
                 }
 
                 override fun onPermissionDenied(p0: MutableList<String>?) {
-
-                    // 권한 거절
-                    Toast.makeText(this@MainActivity, "권한이 없어서 전화연결 실패", Toast.LENGTH_SHORT).show()
+                    //                // 권한이 거부된 경우 수행할 작업
+                Toast.makeText(this@MainActivity, "권한이 없어서 전화연결 실패", Toast.LENGTH_SHORT).show()
                 }
+            }
 
+            checkPermissionsAndMakeCall()
+
+//            val result = TedPermission.create()
+//                .setDeniedMessage("권한이 필요합니다. [설정] > [권한]에서 권한을 허용할 수 있습니다.")
+//                .setPermissions(android.Manifest.permission.CALL_PHONE)
+//                .check()
+        }
+
+
+    }
+    private fun checkPermissionsAndMakeCall() {
+        lifecycleScope.launch {
+            val result = TedPermission.create()
+                .setDeniedMessage("권한이 필요합니다. [설정] > [권한]에서 권한을 허용할 수 있습니다.")
+                .setPermissions(android.Manifest.permission.CALL_PHONE)
+                .check()
+
+            if (result.isGranted) {
+                // 권한이 허용된 경우 수행할 작업
+                val myUri = Uri.parse("tel:010-6592-3201")
+                val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                startActivity(myIntent)
+            } else {
+                // 권한이 거부된 경우 수행할 작업
+                Toast.makeText(this@MainActivity, "권한이 없어서 전화연결 실패", Toast.LENGTH_SHORT).show()
             }
         }
     }
